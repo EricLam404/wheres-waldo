8e9e7032dbb9cca0f4d8af5684d507439805bfed
EricLam404 <lameric9888@gmail.com>
2023-02-12 19:53:58 -0500

    Initial website

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# wheres-waldo
+wheres-waldo
+A library book catalog built with React using Firebase for the backend.
+
+Live server:
+
+This is a solution to the Where's Waldo project, a component of The Odin Project full stack web development course.
---
+++ src/App.js
@@ -0,0 +1,26 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    </div>
+  );
+}
+
+export default App;
+
+
+/*
+  Frontend:
+    Show first map
+    Start timer
+    Display characters
+    Add circle around cursor 
+    Show charcters when clicked
+    Check if clicked character
+    End game if find all characters
+    Add more maps with various difficulties
+  Backend:
+    Save highscores by a nickname
+    Categorize each highscore by map
+*/
