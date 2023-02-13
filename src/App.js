92728c4339583c6497a0e402015ef3a0e55d63c9
EricLam404 <lameric9888@gmail.com>
2023-02-13 14:27:42 -0500

    Added characters

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import './App.css';
-import map from './pics/maps/beach.jpeg';
 import Timer from './components/timer';
+import map from './pics/maps/beach.jpeg';
+import waldo from './pics/characters/waldo.jpeg';
+import wenda from './pics/characters/wenda.png';
+import whitebeard from './pics/characters/whitebeard.jpeg';
+import odlaw from './pics/characters/odlaw.jpeg';
 
 function App() {
   return (
@@ -11,6 +15,24 @@ function App() {
       </div>
       <div className='container'>
         <img src={map} alt="loading map" className='map'/>
+        <div className='characters'>
+        <div className='waldo'>
+          <div className='character-name'>Waldo</div>
+          <img src={waldo} alt='waldo'/>
+        </div>
+        <div className='wenda'>
+          <div className='character-name'>Wenda</div>
+          <img src={wenda} alt='wenda'/>
+        </div>
+        <div className='whitebeard'>
+          <div className='character-name'>Whitebeard</div>
+          <img src={whitebeard} alt='whitebeard'/>
+        </div>
+        <div className='odlaw'>
+          <div className='character-name'>Odlaw</div>
+          <img src={odlaw} alt='odlaw'/>
+        </div>
+        </div>
       </div>
     </div> 
   );
@@ -21,7 +43,6 @@ export default App;
 
 /*
   Frontend:
-    Start timer
     Display characters
     Add circle around cursor 
     Show charcters when clicked
